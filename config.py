9a9afbb747b32faf9734f70862fc7a4994bae317
hendrikbgr <hendrik@whalar.com>
2021-07-23 14:39:43 +0200

    [TSCR] - Changes to config

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+# Do you want to use a proxy? [true|false]
 use_proxy = 'false'
+
+## LIKE MODULE
+
+# Like tweets from keywords [true|false]
+like_from_keywords = 'true'
+
+# Limits [number of max likes]
+
+# If you have max likes set to 10 
+# and you have 5 keywords each
+# keyword will get 2 likes
+
+max_likes_from_keywords = 10
